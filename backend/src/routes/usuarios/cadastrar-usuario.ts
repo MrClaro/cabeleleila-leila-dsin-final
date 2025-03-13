1fa79716e505c93832645f8d411ce0a89a8bf300
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-13 17:23:49 -0300

    refactor(routes): refatorado rota de cadastro de usuarios para melhor convenção de nomenclatura
    - Corrigido erro de overload na passagem dos parametros para o express

--- backend/src/routes/usuarios/cadastrar-usuario.ts
+++ backend/src/routes/usuarios/cadastrar-usuario.ts
@@ -1,11 +1,11 @@
 import { Request, Response, NextFunction, Router } from "express";
-import CadastraUsuarioController from "../../controllers/cadastra-usuarui-controller";
+import CadastraUsuarioController from "../../controllers/cadastrar-usuario-controller";
 
 const router = Router();
 
 router.post(
 	"/",
-	async (res: Response, req: Request, next: NextFunction): Promise<any> => {
+	async (req: Request, res: Response, next: NextFunction): Promise<any> => {
 		try {
 			const { name, email, password } = req.body;
 
@@ -14,7 +14,7 @@ router.post(
 					response: "Formato da requisição inválida!",
 				});
 			}
-			await CadastraUsuarioController.cadastraUsuario(res, req, next);
+			await CadastraUsuarioController.validarUsuario(req, res, next);
 			return;
 		} catch (error) {
 			return next(error);
