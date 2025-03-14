fc0dda37f1453e8f0091bb983166359c930015aa
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-14 13:09:31 -0300

    feat(services): criado service de consulta de usuarios

---
+++ backend/src/services/usuarios/consultarUsuarioService.ts
@@ -0,0 +1,49 @@
+import { Prisma, PrismaClient, user as User } from "@prisma/client";
+import createError from "http-errors";
+
+const prisma = new PrismaClient();
+
+class ConsultaUsuarioService {
+	async consultarUsuarios(
+		skip?: number,
+		take?: number,
+		where?: Prisma.userWhereInput,
+	): Promise<User[]> {
+		try {
+			const users = prisma.user.findMany();
+			return users;
+		} catch (error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				throw createError(500, "Erro ao acessar o banco de dados.");
+			} else if (error instanceof createError.HttpError) {
+				throw error;
+			} else {
+				console.error("Erro interno:", error);
+				throw createError(500, "Erro interno do servidor.");
+			}
+		}
+	}
+	async consultarUsuarioPorId(id: number): Promise<User> {
+		try {
+			const user = prisma.user.findUniqueOrThrow({
+				where: {
+					id: id,
+				},
+			});
+			if (!user) {
+				throw createError(404, "Usuário não encontrado");
+			}
+			return user;
+		} catch (error) {
+			if (error instanceof Prisma.PrismaClientKnownRequestError) {
+				throw createError(500, "Erro ao acessar o banco de dados.");
+			} else if (error instanceof createError.HttpError) {
+				throw error;
+			} else {
+				console.error("Erro interno:", error);
+				throw createError(500, "Erro interno do servidor.");
+			}
+		}
+	}
+}
+export default new ConsultaUsuarioService();
