9999981734db5d5ac22d224bff6a49711260d4b5
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-14 13:01:02 -0300

    feat(routes): criado route de consutla de usuarios

---
+++ backend/src/routes/usuarios/consultarUsuarioRoute.ts
@@ -0,0 +1,37 @@
+import express, { Router, Response, Request, NextFunction } from "express";
+import ConsultarUsuarioController from "../../controllers/usuarios/consultarUsuarioController";
+
+const router = Router();
+router.use(express.urlencoded({ extended: true }));
+router.use(express.json());
+
+router.get(
+	"/",
+	async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+		try {
+			await ConsultarUsuarioController.validarConsulta(req, res, next);
+			return;
+		} catch (error) {
+			return next(error);
+		}
+	},
+);
+
+router.get(
+	"/:id",
+	async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+		try {
+			const { id } = req.params;
+			if (!id) {
+				return res.status(404).json({
+					response: "Formato da requisição inválida!",
+				});
+			}
+			await ConsultarUsuarioController.validarConsultaPorId(req, res, next);
+			return;
+		} catch (error) {
+			return next(error);
+		}
+	},
+);
+export default router;
