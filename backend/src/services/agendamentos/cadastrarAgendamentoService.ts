938b6dbcaef9386b0dff3795e0aaa6332b458cf2
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-14 20:50:24 -0300

    refactor_feat(services): alterado nome do service de atendimento para agendamento seguindo melhor tradução
    - Alterado nome de variaveis para melhor convenção
    - Alterado retorno da promise para Appointment seguindo as boas práticas
    do typescript
    - Melhor tratamento de dados

--- backend/src/services/agendamentos/cadastrarAgendamentoService.ts
+++ backend/src/services/agendamentos/cadastrarAgendamentoService.ts
@@ -1,15 +1,21 @@
-import { Prisma, PrismaClient } from "@prisma/client";
+import {
+	Prisma,
+	PrismaClient,
+	appointment as Appointment,
+	status as Status,
+} from "@prisma/client";
 import createError from "http-errors";
 
 const prisma = new PrismaClient();
 
-class CadastrarAtendimentoService {
-	async cadastrarAtendimento(
+class CadastrarAgendamentoService {
+	async cadastrarAgendamento(
 		client_id: number,
 		user_id: number,
+		status: string,
 		formatedDate: Date,
 		services: { serviceId: number; quantity?: number; observations?: string }[],
-	): Promise<any> {
+	): Promise<Appointment> {
 		try {
 			const clienteExistente = await prisma.user.findUniqueOrThrow({
 				where: {
@@ -19,10 +25,21 @@ class CadastrarAtendimentoService {
 			if (!clienteExistente) {
 				throw createError(404, "Cliente com esse id não existente");
 			}
-			const novoAtendimento = await prisma.appointment.create({
+			let parsedStatus: Status = Status.SCHEDULED;
+			if (status) {
+				const upperStatus = status.toUpperCase();
+				if (Object.values(Status).includes(upperStatus as Status)) {
+					parsedStatus = upperStatus as Status;
+				} else {
+					throw createError(400, "Status inválido.");
+				}
+			}
+
+			const novoAgendamento = await prisma.appointment.create({
 				data: {
 					client_id,
 					user_id,
+					status: parsedStatus,
 					date_time: formatedDate,
 					services: {
 						create: services.map((service) => ({
@@ -33,7 +50,10 @@ class CadastrarAtendimentoService {
 					},
 				},
 			});
-			return novoAtendimento;
+			if (novoAgendamento) {
+				return novoAgendamento;
+			}
+			throw createError(404, "Erro ao criar novo agendamento");
 		} catch (error) {
 			if (error instanceof Prisma.PrismaClientKnownRequestError) {
 				console.log(error);
@@ -48,4 +68,4 @@ class CadastrarAtendimentoService {
 	}
 }
 
-export default new CadastrarAtendimentoService();
+export default new CadastrarAgendamentoService();
