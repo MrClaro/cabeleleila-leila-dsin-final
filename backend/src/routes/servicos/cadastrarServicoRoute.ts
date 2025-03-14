871ff4c67ca0b52ad48bd4b28968876317602f40
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-14 20:33:06 -0300

    refactor(routes): refatorado route de cadastro de serviços
    - Alterado retorno do promise seguindo as boas práticas do typescript
    - Alterado nome da variavel de chamada do controller de cadastro de
    serviços

--- backend/src/routes/servicos/cadastrarServicoRoute.ts
+++ backend/src/routes/servicos/cadastrarServicoRoute.ts
@@ -1,5 +1,5 @@
 import express, { Router, Response, Request, NextFunction } from "express";
-import CadastraServicoController from "../../controllers/servicos/cadastrarServicoController";
+import CadastrarServicoController from "../../controllers/servicos/cadastrarServicoController";
 
 const router = Router();
 
@@ -8,16 +8,16 @@ router.use(express.urlencoded({ extended: true }));
 
 router.post(
 	"/",
-	async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+	async (req: Request, res: Response, next: NextFunction): Promise<void> => {
 		try {
 			const { name, price } = req.body;
 
 			if (!name || !price) {
-				return res.status(404).json({
+				res.status(404).json({
 					response: "Formato da requisição inválida!",
 				});
 			}
-			await CadastraServicoController.validarServico(req, res, next);
+			await CadastrarServicoController.validarCadastro(req, res, next);
 			return;
 		} catch (error) {
 			return next(error);
