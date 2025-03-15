82512e768f4ae931b3a7c0c1af9eeda6fafbd7cb
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-14 22:27:45 -0300

    feat(utils): criado classe utilitaria para geração de tokens jwt

---
+++ backend/src/utils/jwt.ts
@@ -0,0 +1,72 @@
+import jwt from "jsonwebtoken";
+import createError from "http-errors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+interface TokenPayload {
+	id: string;
+	name: string;
+	role: string;
+	exp: number;
+}
+
+// Valida se a Secret key foi definida no .env
+if (!process.env.ACCESS_TOKEN_SECRET) {
+	console.error("Erro: ACCESS_TOKEN_SECRET não definido no ambiente.");
+	process.exit(1);
+}
+
+// Secret Key para assinar e verificar tokens
+const accessTokenSecret: string = process.env.ACCESS_TOKEN_SECRET;
+
+export default {
+	/**
+	 * Assina e gera um token JWT contendo as informações do usuário.
+	 * @param payload - Dados do usuário a serem incluídos no token
+	 * @param options - Opções adicionais para a geração do token (ex.: expiresIn)
+	 * @returns Uma Promise que resolve para o token JWT assinado
+	 */
+	signAccessToken(
+		payload: { name: string; role: string },
+		options?: jwt.SignOptions,
+	): Promise<string> {
+		return new Promise((resolve, reject) => {
+			jwt.sign(
+				payload,
+				accessTokenSecret,
+				options || {},
+				(err: Error | null, token?: string) => {
+					if (err) {
+						return reject(
+							createError.InternalServerError("Erro ao gerar token"),
+						);
+					}
+					resolve(token as string);
+				},
+			);
+		});
+	},
+
+	/**
+	 * Verifica e decodifica um token JWT.
+	 * @param token - O token JWT a ser verificado
+	 * @returns Uma Promise que resolve para o payload do token se válido
+	 */
+	verifyAccessToken(token: string): Promise<TokenPayload> {
+		return new Promise((resolve, reject) => {
+			jwt.verify(
+				token,
+				accessTokenSecret,
+				(err: Error | null, payload?: jwt.JwtPayload | string) => {
+					if (err) {
+						const message =
+							err.name === "JsonWebTokenError" ? "Unauthorized" : err.message;
+						return reject(createError.Unauthorized(message));
+					}
+					resolve(payload as TokenPayload);
+				},
+			);
+		});
+	},
+};
