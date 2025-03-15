ad67050ef5c20b0e1a0f784be128386a8fd0da05
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-15 19:16:59 -0300

    feat(controllers): criado função para validar usuario por email

--- backend/src/controllers/usuarios/consultarUsuarioController.ts
+++ backend/src/controllers/usuarios/consultarUsuarioController.ts
@@ -2,6 +2,7 @@ import Joi from "joi";
 import createError from "http-errors";
 import { Request, Response, NextFunction } from "express";
 import ConsultarUsuarioService from "../../services/usuarios/consultarUsuarioService";
+import { usuario as Usuario } from "@prisma/client";
 class ConsultarUsuarioController {
 	async validarConsulta(
 		req: Request,
@@ -49,6 +50,52 @@ class ConsultarUsuarioController {
 				return next(createError(400, error.details[0].message));
 			}
 			next(createError(500, "Erro interno do servidor"));
+		}
+	}
+	async validarConsultaPorEmail(
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	): Promise<Usuario | null> {
+		try {
+			const { email } = req.body;
+			const consultaSchema = Joi.object({
+				email: Joi.string()
+					.email({
+						minDomainSegments: 2,
+						tlds: { allow: ["com", "net"] },
+					})
+					.required()
+					.messages({
+						"string.email.minDomainSegments":
+							"O email deve estar completo com domínio.",
+						"string.email.tlds.allow": "O email deve terminar em .com ou .net.",
+						"string.required": "O email é obrigatorio",
+					}),
+			});
+			const { error } = consultaSchema.validate({
+				email,
+			});
+			if (error) {
+				next(createError(400, error.details[0].message));
+				return null;
+			}
+			const usuario =
+				await ConsultarUsuarioService.consultarUsuarioPorEmail(email);
+
+			if (!usuario) {
+				next(createError(404, "Usuário não encontrado"));
+				return null;
+			}
+
+			return usuario;
+		} catch (error) {
+			if (error instanceof Joi.ValidationError) {
+				next(createError(400, error.details[0].message));
+			} else {
+				next(createError(500, "Erro interno do servidor"));
+			}
+			return null;
 		}
 	}
 }
