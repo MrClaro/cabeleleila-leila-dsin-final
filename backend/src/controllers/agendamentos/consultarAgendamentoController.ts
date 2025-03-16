2cf58f09a31e103ddf9e70189db59024e5d6a91a
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-16 01:29:05 -0300

    feat(controllers): adicionado consulta de agendamentos por data

--- backend/src/controllers/agendamentos/consultarAgendamentoController.ts
+++ backend/src/controllers/agendamentos/consultarAgendamentoController.ts
@@ -3,7 +3,7 @@ import createError from "http-errors";
 import ConsultarAgendamentoService from "../../services/agendamentos/consultarAgendamentoService";
 import Joi from "joi";
 
-class ConsultarAgendamentoControllerr {
+class ConsultarAgendamentoController {
 	async validarConsulta(
 		req: Request,
 		res: Response,
@@ -54,6 +54,52 @@ class ConsultarAgendamentoControllerr {
 			next(createError(500, "Erro interno do servidor"));
 		}
 	}
+	async validarConsultaPorData(
+		req: Request,
+		res: Response,
+		next: NextFunction,
+	): Promise<void> {
+		try {
+			const { dataInicio, dataFim } = req.body;
+
+			const consultaSchema = Joi.object({
+				dataInicio: Joi.date().optional(),
+				dataFim: Joi.date().iso().required().messages({
+					"date.required": "A data final é obrigatória",
+					"date.iso": "Data inválida. O formato aceito é YYYY-MM-DD",
+				}),
+			});
+
+			const { error } = consultaSchema.validate({ dataInicio, dataFim });
+			if (error) {
+				return next(createError(400, error.details[0].message));
+			}
+
+			const dataInicioFormatada = dataInicio ? new Date(dataInicio) : undefined;
+			const dataFimFormatada = new Date(dataFim);
+
+			if (
+				isNaN(dataFimFormatada.getTime()) ||
+				(dataInicioFormatada && isNaN(dataInicioFormatada.getTime()))
+			) {
+				return next(createError(400, "Data inválida."));
+			}
+
+			const agendamentos =
+				await ConsultarAgendamentoService.consultarAgendamentosPorData(
+					dataFimFormatada,
+					dataInicioFormatada,
+				);
+
+			res.status(200).json({ agendamentos });
+		} catch (error) {
+			if (error instanceof createError.HttpError) {
+				return next(error);
+			}
+			console.error("Erro interno:", error);
+			next(createError(500, "Erro interno do servidor"));
+		}
+	}
 }
 
-export default new ConsultarAgendamentoControllerr();
+export default new ConsultarAgendamentoController();
