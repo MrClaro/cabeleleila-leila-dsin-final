b64e22a653df8c77aaed085e25c63360bbb63647
MrClaro <adryan.contatoprofissional@gmail.com>
2025-03-16 01:28:44 -0300

    refactor(controllers): refatorado forma antiga de tratamento de datas para cadastro  de agendamentos

--- backend/src/controllers/agendamentos/cadastrarAgendamentoController.ts
+++ backend/src/controllers/agendamentos/cadastrarAgendamentoController.ts
@@ -12,9 +12,8 @@ class CadastrarAgendamentoController {
 	): Promise<void> {
 		try {
 			const { usuario_id, status, data_hora, servicos } = req.body;
-
 			const cadastroSchema = Joi.object({
-				data_hora: Joi.date().min("now").required().messages({
+				data_hora: Joi.date().greater("now").required().messages({
 					"date.min": "A data minima aceita é a data atual",
 				}),
 				servicos: Joi.array()
@@ -43,27 +42,19 @@ class CadastrarAgendamentoController {
 				return next(error);
 			}
 
-			const dataFormatada = formataData(data_hora);
-			if (!dataFormatada) {
-				return next(
-					createError(400, "Data inválida ou fora do intervalo permitido."),
-				);
-			}
 			const temUsuarioId = usuario_id ? usuario_id : null;
 
 			try {
-				if (dataFormatada) {
-					const dataFinal = new Date(dataFormatada);
-					const agendamento =
-						await CadastrarAgendamentoService.cadastrarAgendamento(
-							temUsuarioId,
-							status,
-							dataFinal,
-							servicos,
-						);
-					if (agendamento) {
-						res.status(201).json({ response: agendamento });
-					}
+				const dataFinal = new Date(data_hora);
+				const agendamento =
+					await CadastrarAgendamentoService.cadastrarAgendamento(
+						temUsuarioId,
+						status,
+						dataFinal,
+						servicos,
+					);
+				if (agendamento) {
+					res.status(201).json({ response: agendamento });
 				}
 			} catch (serviceError) {
 				return next(serviceError);
